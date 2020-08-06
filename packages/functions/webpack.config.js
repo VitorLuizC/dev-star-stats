b8b9a51d2471fa5bced30d7ddfd5727964285a30
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2020-08-06 02:47:16 -0300

    :wrench: add dotenv & webpack for functions

---
+++ packages/functions/webpack.config.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const path = require("path");
+const DotEnvPlugin = require('webpack-dotenv');
+
+/**
+ * @type {import('webpack').ConfigurationFactory}
+ */
+const configuration = mode => ({
+  mode,
+  target: 'node',
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  externals: /^[^\.]/,
+  resolve: {
+    extensions: [".ts", ".js", ".json"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    libraryTarget: 'commonjs',
+  },
+  plugins: [
+    new DotEnvPlugin({
+      path: './.env',
+      safe: true,
+      expand: true,
+    })
+  ]
+});
+
+module.exports = configuration;
